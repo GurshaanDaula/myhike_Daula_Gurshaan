9d94e42449b38686d77b7302a8e742498548569a
Gurshaan Daula <gurshaandaula@gmail.com>
2024-03-05 09:59:48 -0800

    fhewiufs

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyD-wgqDhp83PRM7LtwnN_JvnIkDH4zFEYs",
+    authDomain: "comp-1800---202410.firebaseapp.com",
+    projectId: "comp-1800---202410",
+    storageBucket: "comp-1800---202410.appspot.com",
+    messagingSenderId: "644170316503",
+    appId: "1:644170316503:web:c3ee0816a5f7f449b20e92"
 };
 
 //--------------------------------------------
